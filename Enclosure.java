cefb80d51f8784c90a106e7601f78785e10d50d9
Tracey Le Roux <27038815@sun.ac.za>
2025-04-12 11:19:09 +0000

    more

--- Enclosure.java
+++ Enclosure.java
@@ -3,5 +3,50 @@
  */
 
 public class Enclosure {
-	
+	/* consists of enclousures for animals */
+
+	/* but also store food storage depot info */
+	/* if this  */
+	boolean empty = false;
+
+	int x;
+	int y;
+	int z;
+
+	int importance = 0;
+
+	int diet;
+
+	public Enclosure() {
+		/* empty by default */
+		empty = true;
+	}
+
+	/**
+	 * Create enclosure for food storage object
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @param diet
+	 */
+	public Enclosure(int x1, int y1, int z1, int diet1) {
+		x = x1;
+		y = y1;
+		z = z1;
+
+		diet = diet1;
+	}
+
+	/**
+	 * Enclosure for animals
+	 * MAIN PURPOSE HERE
+	 * @param z
+	 * @param importance
+	 * @param diet
+	 */
+	public Enclosure(int z1, int importance1, int diet1) {
+		z = z1;
+		importance = importance1;
+		diet = diet1;
+	}
 }
--- Zoo.java
+++ Zoo.java
@@ -5,19 +5,48 @@
 public class Zoo {
 	/* have three different 2D matrices */
 
+	int zoo_x;
+	int zoo_y;
+	int zoo_z;
+
+	Enclosure herbis [][];
+	Enclosure carnis [][];
+	Enclosure omnis [][];
+
+	Enclosure food_depots[] = new Enclosure[3];
+	
+	int drone_x;
+	int drone_y;
+	int drone_z;
+
+	static int iter = 0;
 
 	public Zoo(int x, int y, int z) {
 		/* store zoo coordinates */
+		zoo_x = x;
+		zoo_y = y;
+		zoo_z = z;
+
+		herbis = new Enclosure[x][y];
+
+
+		/*for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				
+			}
+		}*/
 	}
 
 	/**
-	 * Drone Depot COords
+	 * Drone Depot Coords
 	 * @param x
 	 * @param y
 	 * @param z
 	 */
 	public void setDroneDepot(int x, int y, int z) {
-
+		drone_x = x;
+		drone_y = y;
+		drone_z = z;
 	}
 
 	/**
@@ -28,7 +57,9 @@ public class Zoo {
 	 * @param diet
 	 */
 	public void setFoodCoord(int x, int y, int z, final int diet) {
-		
+		Enclosure food_enclosure = new Enclosure(x, y, z, diet);
+		food_depots[iter] = food_enclosure;
+		iter++;
 	}
 
 	/**
