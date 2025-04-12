fae56045f5825b731b15b95bd60b03cd80c7f059
Tracey Le Roux <27038815@sun.ac.za>
2025-04-12 10:48:12 +0000

    skeleton

--- Zoo.java
+++ Zoo.java
@@ -3,5 +3,53 @@
  * the grid
  */
 public class Zoo {
-	
+	/* have three different 2D matrices */
+
+
+	public Zoo(int x, int y, int z) {
+		/* store zoo coordinates */
+	}
+
+	/**
+	 * Drone Depot COords
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
+	public void setDroneDepot(int x, int y, int z) {
+
+	}
+
+	/**
+	 * Store details of ONE food STORAGE DEPOT
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @param diet
+	 */
+	public void setFoodCoord(int x, int y, int z, final int diet) {
+		
+	}
+
+	/**
+	 * Store details for one enclosure
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @param importance
+	 * @param diet
+	 */
+	public void setEnclosureCoord(int x, int y, int z, int importance, final int diet) {
+
+	}
+
+	/**
+	 * Store details of ONE dedad zone
+	 * @param x
+	 * @param y
+	 * @param r
+	 */
+	public void setDeadZone(int x, int y, int r) {
+
+	}
 }
