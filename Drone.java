d7d7d7ce176d403e9a0600a3bdf7515d3ec98c16
26927918 <26927918@sun.ac.za>
2025-04-12 11:54:51 +0200

    Whoop

---
+++ Drone.java
@@ -0,0 +1,8 @@
+public class Drone {
+
+    int distance_capacity;
+
+    public Drone(int dc) {
+        distance_capacity = dc;
+    }
+}
---
+++ ZooManager.java
@@ -0,0 +1,76 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
+
+public class ZooManager {
+
+    public static Zoo zoo;
+    public static int distance_capacity;
+    public static Drone drone;
+
+    public static void main(String[] args) {
+        readFile();
+    }
+
+    private static void readFile() {
+        try {
+
+            // Zoo Coords
+            Scanner scLine = new Scanner(new File("1.txt"));
+            String[] z_coords = scLine.nextLine().split(",");
+            zoo = new Zoo(Integer.parseInt(z_coords[0].replace("(", "")), Integer.parseInt(z_coords[1]),
+                    Integer.parseInt(z_coords[2].replace(")", "")));
+
+            // Depot Coords
+            String[] depot_coords = scLine.nextLine().split(",");
+            zoo.setDroneDepot(Integer.parseInt(depot_coords[0].replace("(", "")), Integer.parseInt(depot_coords[1]),
+                    Integer.parseInt(depot_coords[2].replace(")", "")));
+
+            // Distance Capacity
+            distance_capacity = Integer.parseInt(scLine.nextLine());
+            drone = new Drone(distance_capacity);
+
+            // Food Storages Coords
+            String food_storages_str = scLine.nextLine();
+            food_storages_str = food_storages_str.substring(1, (food_storages_str.length() - 2));
+
+
+            String[] food_storages = food_storages_str.split("\\),");
+
+            for (int i = 0; i < food_storages.length; i++) {
+                String[] temp_food = food_storages[i].split(",");
+                int diet = 0;
+                if (temp_food[3].equals("c")) {
+                    diet = 1;
+                } else if (temp_food[3].equals("o")) {
+                    diet = 2;
+                }
+                zoo.setFoodCoord(Integer.parseInt(temp_food[0].replace("(", "")), Integer.parseInt(temp_food[1]),
+                        Integer.parseInt(temp_food[2]), diet);
+            }
+
+            // Enclosures
+            String enclosers_str = scLine.nextLine();
+            enclosers_str = enclosers_str.substring(1, enclosers_str.length() - 2);
+
+            String[] enclosures = enclosers_str.split("\\),");
+
+            for (int i = 0; i < enclosures.length; i++) {
+                String[] temp_encl = enclosures[i].split(",");
+
+                int diet = 0;
+                if (temp_encl[3].equals("c")) {
+                    diet = 1;
+                } else if (temp_encl[3].equals("o")) {
+                    diet = 2;
+                }
+
+                zoo.setEnclosureCoord(Integer.parseInt(temp_encl[0].replace("(", "")), Integer.parseInt(temp_encl[1]),
+                        Integer.parseInt(temp_encl[2]), Float.parseFloat(temp_encl[3]), diet);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
+}
