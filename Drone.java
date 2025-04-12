32bd5fe88da94c13880aae4e047ea51a4273c89a
26927918 <26927918@sun.ac.za>
2025-04-12 11:57:09 +0200

    Getters and Setters for Drone

--- Drone.java
+++ Drone.java
@@ -5,4 +5,12 @@ public class Drone {
     public Drone(int dc) {
         distance_capacity = dc;
     }
+
+    public void setCapacity(int new_dc) {
+        distance_capacity = new_dc;
+    }
+
+    public int getCapacity() {
+        return distance_capacity;
+    }
 }
